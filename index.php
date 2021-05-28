d8d1e99b4033b96d65439186a311e3f4b7b08dab
jonasm1307 <joma98@gmx.de>
2021-05-28 18:40:56 +0200

    Viel Spass

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include_once 'includes/dbh.inc.php';
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    
+</head>
+
+<body>
+    hallo test!
+    <div class="v3_38"><span class="v3_37"></span></div>
+</body>
+
+</html>
+
+
+<!DOCTYPE html>
+<html>
